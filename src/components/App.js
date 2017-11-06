97fb36491d584967baf4e6de3a38680546a9f2c8
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-11-06 20:13:53 +0200

    lesson11. Debugging React Apps

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
 import 'bootstrap/dist/css/bootstrap.css'
 
-class App extends Component {
+class App extends PureComponent {
     state = {
         reverted: false
     }
-
     render() {
+        console.log('---', 1)
         return (
             <div className="container">
                 <div className="jumbotron">
@@ -17,14 +17,16 @@ class App extends Component {
                         <button className="btn" onClick = {this.revert}>Revert</button>
                     </h1>
                 </div>
-                <ArticleList articles={this.state.reverted ? articles.reverse() : articles}/>
+                <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>
             </div>
         )
     }
 
-    revert = () => this.setState({
-        reverted: !this.state.reverted
-    })
+    revert = () => {
+        this.setState({
+            reverted: !this.state.reverted
+        })
+    }
 }
 
 export default App
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import Article from '../Article'
 import './style.css'
 
-export default function ArticleList({ articles }) {
-    const articleElements = articles.map((article, index) =>
-        <li key = {article.id} className="article-list__li">
-            <Article article = {article} defaultOpen = {index === 0}/>
-        </li>
-    )
-    return (
-        <ul>
-            {articleElements}
-        </ul>
-    )
+export default class ArticleList extends PureComponent {
+    render() {
+        console.log('---', 2)
+        const articleElements = this.props.articles.map((article, index) =>
+            <li key = {article.id} className="article-list__li">
+                <Article article = {article} defaultOpen = {index === 0}/>
+            </li>
+        )
+        return (
+            <ul>
+                {articleElements}
+            </ul>
+        )
+    }
 }
